969b929c26da64b9c10414721f9002ddb98cedbc
zPierreB <pierre.briere1998@gmail.com>
2023-09-05 17:34:06 +0200

    add few chnages, need to check for count

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import axios from 'axios'
 import './App.css'
+import RollDice from './rollDice/RollDice.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,10 +11,19 @@ function App() {
     axios.get('http://localhost:8000/patries')
       .then((res) => {
         setPatries(res.data.patries)
+        console.log(patries)
       })
       .catch((error) => {
         console.log(error)
       })
+  }
+
+  // const update = async (number) => {
+  //   axios.put('http://localhost:8000/patries/')
+  // }
+
+  const checkWin = (winResult) => {
+    setCount(winResult)
   }
 
   useEffect(() => {
@@ -24,31 +32,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Wanna win a patrie? Roll the dice!</h1>
+      <h2>Pastries left: {count} </h2>
       <div>
         {patries.length > 0 && patries.map((patrie) =>
-          <p>{patrie.name}</p>
+          <li key={patrie._id}>
+            <p><strong>{patrie.name}</strong>, Amount: {patrie.number} <button onClick={() => setCount((count) => count - 1)} disabled={count <= 0}>Take the {patrie.name} </button></p>
+          </li>
         )}
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RollDice checkWin={checkWin} />
     </>
   )
 }
---
+++ frontend/src/rollDice/RollDice.jsx
@@ -0,0 +1,150 @@
+import { useState, useEffect } from 'react'
+
+function RollDice({ checkWin }) {
+    const faces = 6
+
+    const [rolls, setRolls] = useState([])
+    const [resultMessage, setResultMessage] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(false)
+    const [pastryWin, setPastryWin] = useState(0)
+
+    const makeOneRoll = async () => {
+        const arrayRolls = []
+
+        for (let i = 0; i < 5; i++) {
+            const number = Math.floor((Math.random() * faces) + 1)
+            arrayRolls.push(number)
+        }
+        setRolls([1, 2, 2, 4, 4])
+        setBtnDisabled(true)
+        await checkWin(pastryWin)
+    }
+
+    let array1 = []
+    let array2 = []
+    let array3 = []
+    let array4 = []
+    let array5 = []
+    let array6 = []
+
+    let doublePair = []
+    let carre = []
+    let yahtzee = []
+
+    const checkNumbers = (rolls) => {
+        rolls.filter((roll) => {
+            if (roll == 1) {
+                array1.push(roll)
+                if (array1.length == 2 || array1.length == 3) {
+                    doublePair.unshift('pair')
+                }
+                if (array1.length == 4) {
+                    carre.unshift('carre')
+                }
+                if (array1.length == 5) {
+                    yahtzee.unshift('yahtzee')
+                }
+            }
+            if (roll == 2) {
+                array2.push(roll)
+                if (array2.length == 2 || array2.length == 3) {
+                    doublePair.unshift('pair')
+                }
+                if (array2.length == 4) {
+                    carre.unshift('carre')
+                }
+                if (array2.length == 5) {
+                    yahtzee.unshift('yahtzee')
+                }
+            }
+            if (roll == 3) {
+                array3.push(roll)
+                if (array3.length == 2 || array3.length == 3) {
+                    doublePair.unshift('pair')
+                }
+                if (array3.length == 4) {
+                    carre.unshift('carre')
+                }
+                if (array3.length == 5) {
+                    yahtzee.unshift('yahtzee')
+                }
+            }
+            if (roll == 4) {
+                array4.push(roll)
+                if (array4.length == 2 || array4.length == 3) {
+                    doublePair.unshift('pair')
+                }
+                if (array4.length == 4) {
+                    carre.unshift('carre')
+                }
+                if (array4.length == 5) {
+                    yahtzee.unshift('yahtzee')
+                }
+            }
+            if (roll == 5) {
+                array5.push(roll)
+                if (array5.length == 2 || array5.length == 3) {
+                    doublePair.unshift('pair')
+                }
+                if (array5.length == 4) {
+                    carre.unshift('carre')
+                }
+                if (array5.length == 5) {
+                    yahtzee.unshift('yahtzee')
+                }
+            }
+            if (roll == 6) {
+                array6.push(roll)
+                if (array6.length == 2 || array6.length == 3) {
+                    doublePair.unshift('pair')
+                }
+                if (array6.length == 4) {
+                    carre.unshift('carre')
+                }
+                if (array6.length == 5) {
+                    yahtzee.unshift('yahtzee')
+                }
+            }
+        })
+    }
+
+    const checkResults = (doublePair, carre, yahtzee) => {
+        if (yahtzee.length > 0) {
+            setResultMessage('You win 3 pastries!')
+            setPastryWin(3)
+            return
+        }
+        if (carre.length > 0) {
+            setResultMessage('You win 2 pastries!')
+            setPastryWin(2)
+            return
+        }
+        if (doublePair.length > 1 && doublePair.length < 4) {
+            setResultMessage('You win 1 pastry!')
+            setPastryWin(1)
+            return
+        }
+        else {
+            setResultMessage('You lose...')
+        }
+    }
+
+    useEffect(() => {
+        checkResults(doublePair, carre, yahtzee)
+    }, [doublePair, carre, yahtzee])
+
+    checkNumbers(rolls)
+
+    console.log('help', pastryWin)
+
+    return (
+        <div>
+            <h1>{resultMessage}</h1>
+            <p>{rolls}</p>
+            <input type='submit' value='Roll dice' onClick={() => makeOneRoll()} />
+        </div>
+    )
+
+}
+
+export default RollDice
